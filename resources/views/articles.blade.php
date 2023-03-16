8e46b170740def293882491e0c44dbbac946a002
Sem <sembloemberg@gmail.com>
2023-03-16 15:01:08 +0100

    added 3 buttons to homepage view

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
-  <h1 class="text-red-500">testtest</h1>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <a href="/create-story" class="bg-red-500 p-3 rounded-lg mx-2">Create story</a>
+      <a href="/submitted-stories" class="bg-green-500 p-3 rounded-lg mx-2">Approve stories</a>
+      <a href="/rejected-stories" class="bg-red-500 p-3 rounded-lg mx-2">Rejected stories</a>
+    </div>
+  </div>
 </x-layout>
